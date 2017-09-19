9b28000438275f9aa3073fa5f073d3d61e158540
Shashanka Nataraj <shasn@cisco.com>
2017-09-19 12:16:43 +0530

    Notes flow is almost done, works with books & pages. Only need to set the MD to the page object in firebase now

--- src/components/Notes/NotesPage.vue
+++ src/components/Notes/NotesPage.vue
@@ -1,6 +1,16 @@
 <template>
-  <v-layout row text-xs-center>
-    <v-flex xs3 sm3 column offset-xs1>
+<v-container grid-list-md text-xs-center>
+  <v-layout row wrap>
+    <v-flex row xs12 wrap>
+      <v-breadcrumbs row xs12 icons divider="forward">
+        <v-breadcrumbs-item
+          v-for="path in currentUserPath" :key="path"
+        >
+          {{ path }}
+        </v-breadcrumbs-item>
+      </v-breadcrumbs>
+    </v-flex>
+    <v-flex xs12 sm12 column v-if="!isBookSelected">
       <v-card row class="purple white--text">
         <v-list>
           <v-subheader>
@@ -37,24 +47,76 @@
         </v-list>
       </v-card>
     </v-flex>
-    <v-flex xs7 sm7 column>
-      <v-card>
-          <textarea :value="currentNoteMarkdown" @input="update"></textarea>
+    <v-flex xs12 sm12 column v-if="isBookSelected">
+      <v-card row class="purple white--text">
+        <v-list>
+          <v-subheader>
+            <v-icon>
+              page
+            </v-icon>
+            Pages
+          </v-subheader>
+          <v-divider></v-divider>
+          <v-list-tile avatar v-for="note in notesInCurrentBook" v-bind:key="note.title" v-bind:class="selectedBookKey === note['.key']?'deep-purple lighten-4':''" @click="selectPage(note)">
+            <v-list-tile-content>
+              <v-list-tile v-text="note.title"></v-list-tile>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-footer v-bind:class="[isAddingPage?'footer-adding-book':'footer-normal']">
+            <v-flex xs12>
+              <v-spacer v-if="!isAddingPage"></v-spacer>
+              <v-btn flat outline block secondary @click="addNewPage()" v-if="!isAddingPage">
+                <v-icon>
+                  add
+                </v-icon>Add New Page
+              </v-btn>
+              <div v-if="isAddingPage" row wrap>
+                <v-text-field label="New Page Name" column width="50px" v-model="newPageName"></v-text-field>
+                <v-btn @click="savePageNameToBook()">
+                  <v-icon>
+                    check
+                  </v-icon>
+                  Save
+                </v-btn>
+              </div>
+            </v-flex>
+          </v-footer>
+        </v-list>
       </v-card>
     </v-flex>
+    <v-flex xs12 sm12 column v-if="isBookSelected && isPageSelected">
+      <v-layout row>
+        <v-flex column sm3>
+          <v-card row>
+            <v-text-field
+              label="Markdown"
+              multi-line
+              v-model="currentNoteMarkdown"
+              auto-grow
+            ></v-text-field>
+          </v-card>
+        </v-flex>
+        <v-flex column sm9>
+          <v-card row>
+            <div column v-html="compiledMarkdown"></div>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-flex>
   </v-layout>
+  </v-container>
 </template>
+
 <script>
 import Note from './Note'
 import marked from 'marked'
-import _ from 'lodash'
 export default {
   methods: {
-    update: _.debounce(function (e) {
-      this.currentNoteMarkdown = e.target.value
-    }, 300),
     addNewBook () {
       this.isAddingBook = true
+    },
+    addNewPage () {
+      this.isAddingPage = true
     },
     saveBookName () {
       this.isAddingBook = false
@@ -63,8 +125,35 @@ export default {
       })
       this.newBookName = ''
     },
-    selectBook (note) {
-      this.selectedBookKey = note['.key']
+    savePageNameToBook () {
+      this.isAddingPage = false
+      let currentBook = {...this.getCurrentSelectedBook()}
+      let currentPages = currentBook['pages'] || []
+      let newPageObj = {
+        title: this.newPageName,
+        md: '# Hello there!'
+      }
+      currentPages.push(newPageObj)
+      currentBook['pages'] = currentPages
+      this.updateBook(currentBook)
+      this.newPageName = ''
+    },
+    updateBook (book) { // Accepts Updates a book object with the passed book
+      let currentSelectedBookObj = this.getCurrentSelectedBook()
+      let relevantBookKey = currentSelectedBookObj['.key']
+      delete book['.key']
+      this.books.child(relevantBookKey).set(book)
+    },
+    selectBook (book) { // Triggers selection of a book on the UI by setting internal vars
+      this.currentUserPath.push(book.title)
+      this.selectedBookKey = book['.key']
+    },
+    selectPage (page) {
+      this.selectedPageName = page
+    },
+    getCurrentSelectedBook () {
+      return this.booksList
+              .find(book => book['.key'] === this.selectedBookKey, this)
     }
   },
   components: {
@@ -79,14 +168,34 @@ export default {
   computed: {
     compiledMarkdown: function () {
       return marked(this.currentNoteMarkdown, { sanitize: true })
+    },
+    notesInCurrentBook () {
+      let pages
+      if (this.booksList.length > 0) {
+        let currentBook = this.getCurrentSelectedBook()
+        if (currentBook) {
+          pages = currentBook['pages']
+        }
+      }
+      return pages || []
+    },
+    isBookSelected () {
+      return this.selectedBookKey !== ''
+    },
+    isPageSelected () {
+      return this.selectedBookKey !== '' && this.selectedPageName !== ''
     }
   },
   data () {
     return {
-      isAddingBook: false,
-      newBookName: '',
-      selectedBookKey: '',
-      currentNoteMarkdown: '# hello'
+      isAddingBook: false, // Is the user in new book adding mode? Book add form is toggled based on this flag
+      isAddingPage: false, // Same as isAddingBook, but for pages
+      newBookName: '', // New book name being entered by the user
+      newPageName: '', // New page name being entered by the user
+      selectedBookKey: '', // Current clicked books key
+      selectedPageName: '', // Current clicked page name
+      currentNoteMarkdown: '# hello', // Current markdown text being edited
+      currentUserPath: [] // Tracks users selections on the screen mainly to show breadcrumbs
     }
   }
 }
